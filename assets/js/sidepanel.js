4ddc7dad52182681e5b2430ae8e4bcb1c451c16a
JamesH <jwhusc7@gmail.com>
2022-02-10 22:09:34 -0500

    added sticky open/closable side navigation panel

---
+++ assets/js/sidepanel.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "23%";
+    document.getElementById("mySidepanel").style.height = "17%";
+}
+
+  /* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+    document.getElementById("mySidepanel").style.height = "0";
+}
