77f44621cfc322237539f6a56058329e3c71f6a1
JamesH <jwhusc7@gmail.com>
2022-02-13 17:24:28 -0500

    Refactored some of html and css

--- assets/js/sidepanel.js
+++ assets/js/sidepanel.js
@@ -1,7 +1,7 @@
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "19%";
-    document.getElementById("mySidepanel").style.height = "18%";
+    document.getElementById("mySidepanel").style.width = "250px";
+    document.getElementById("mySidepanel").style.height = "315px";
 }
 
   /* Set the width of the sidebar to 0 (hide it) */
